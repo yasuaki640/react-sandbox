443713f392fdad0b997317b8043b501ac56ae57e
yasuaki640 <yasuaki640@gmail.com>
2020-12-05 17:35:59 +0900

    キー名とバリュー名が一緒ならばキー名は省略できる

--- src/components/ColofulMessage.jsx
+++ src/components/ColofulMessage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ColofulMessage = (props) => {
     const {color,children} = props;
     const contentStyle = {
-        color: color,
+        color,
         fontSize: '18px'
     };
     return (
