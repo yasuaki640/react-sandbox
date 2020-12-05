f76d795780f1d5b1ff387131c00484d5826a0b78
yasuaki640 <yasuaki640@gmail.com>
2020-12-05 16:47:18 +0900

    change structure of codes

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <>
+            <h1>Hello world!</h1>
+            <p>人はなぜ生きるのか？</p>
+        </>
+    );
+};
+
+export default App;
