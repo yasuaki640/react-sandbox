86ba6b0b473cfa85dd4107ee305aa97ee9b8f06f
yasuaki640 <yasuaki640@gmail.com>
2020-12-05 16:54:22 +0900

    Learn how to apply CSS in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
 const App = () => {
+    const onClickButton = () => alert();
+
+    const contentStyle = {
+        color: 'blue',
+        fontSize: '18px'
+    };
     return (
         <>
-            <h1>Hello world!</h1>
-            <p>人はなぜ生きるのか？</p>
+            <h1 style={{color: 'red'}}>Hello world!</h1>
+            <p style={contentStyle}>人はなぜ生きるのか？</p>
+            <button onClick={onClickButton}>button</button>
         </>
     );
 };
