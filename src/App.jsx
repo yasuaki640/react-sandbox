9e1dab8ce7413033ca5f3ae065f38c4b0c396c19
yasuaki640 <yasuaki640@gmail.com>
2020-12-05 17:42:10 +0900

    learn use state

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ColofulMessage from './components/ColofulMessage'
 
 const App = () => {
-    const onClickButton = () => alert();
-
+    const onClickCountUp = () => {
+        setNum(num + 1);
+    };
+    const [num, setNum] = useState(100);
 
     return (
         <>
             <h1 style={{color: 'red'}}>Hello world!</h1>
             <ColofulMessage color={"blue"}>深淵を覗くとき</ColofulMessage>
             <ColofulMessage color={"pink"}>深淵もまたこちらを除いているのだ</ColofulMessage>
-            <button onClick={onClickButton}>button</button>
+            <button onClick={onClickCountUp}>Count up</button>
+            <p>{num}</p>
         </>
     );
 };
